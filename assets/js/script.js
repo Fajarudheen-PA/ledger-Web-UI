e32276529c4b03c2cf10b493a8c3f18756a81cd4
Fajarudheen PA <fajarudheen98@gmail.com>
2023-04-02 12:33:04 +0530

    NavBar updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
 $(window).on("scroll", function() {
     if ($(window).scrollTop() > 140) {
         $(".ledger-header").addClass("sticky");
-        $("body").addClass("sticky-soomth");
     } else {
         $(".ledger-header").removeClass("sticky");
-        $("body").removeClass("sticky-soomth");
     }
 });
