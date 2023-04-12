f01192276547361a7933eaaacce7078e41214506
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-04-12 14:37:48 +0530

    updated booking page

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,16 @@ $(window).on("scroll", function() {
         $(".ledger-header").removeClass("sticky");
     }
 });
+
+$(document).ready(function(){
+    $('#datepicker').datepicker({
+      format: 'dd-mm-yyyy',
+      autoclose: true,
+      todayHighlight: true,
+      orientation: 'bottom',
+      templates: {
+        leftArrow: '<i class="glyphicon glyphicon-chevron-left"></i>',
+        rightArrow: '<i class="glyphicon glyphicon-chevron-right"></i>'
+      }
+    });
+  });
